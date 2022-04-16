41198f9733de64b364f1a2dc2bd0147ac3312830
laurimil <laurimilehto@gmail.com>
2022-04-16 11:27:23 +0300

    add lunch menu scroll for food n co

--- components/Fazer.js
+++ components/Fazer.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import { 
   Container,
   Col, 
@@ -11,12 +11,24 @@ import FazerRow from './FazerRow'
 import foodnco from '../data/foodnco'
 
 const Menu = ({fetched,data}) => {
-  console.log(data)
 
+  const fieldRef = React.useRef(null);
+  React.useEffect(() => {
+    if (fieldRef.current) {
+      setTimeout(function(){
+      fieldRef.current.scrollIntoView({
+        behavior: "smooth",
+      });
+    },10000)
+    }
+  }, []);
+
+
+  console.log(data)
   return (
     <Container 
       fluid 
-      style={{height: '80%', margin: 'auto'}}
+      //style={{height: '80%', margin: 'auto'}}
       /* className="d-flex align-items-center justify-content-center flex-column" */
     >
       <TopBar/>
@@ -27,14 +39,19 @@ const Menu = ({fetched,data}) => {
           <Row>{foodnco.open.en}</Row>
         </Col>
         <Col sx={2}> klo. {foodnco.open.time}</Col>
-        <Col sx={2}>
-          <Row>{foodnco.lunch.fi}</Row>
-          <Row>{foodnco.lunch.en}</Row>
-        </Col>
-        <Col sx={2}> klo. {foodnco.lunch.time}</Col>
+        { data.fi.MenusForDays[0].LunchTime ? 
+          <>
+            <Col sx={2}>
+              <Row>{foodnco.lunch.fi}</Row>
+              <Row>{foodnco.lunch.en}</Row>
+            </Col>
+            <Col sx={2}> klo. {data.fi.MenusForDays[0].LunchTime}</Col>
+          </>
+          : 
+          <Col sx={4}>Ravintola suljettu tänään</Col>
+        }
       </Row>
-      <Row 
-        >
+      <Row style={{minHeight: "60vh"}}>
         { data.courses ?
           <>
             <Col>
@@ -45,10 +62,16 @@ const Menu = ({fetched,data}) => {
             </Col>
           </>
           : 
-          fetched ? <div style={{padding:"50px"}}>{"Food 'n' Co ruokalista ei ole saatavilla"}</div> : <Spinner animation="border" role="status" variant="dark" />
+          fetched ? 
+            <div style={{padding:"50px", textAlign: "center"}}>{"Food 'n' Co ruokalista ei ole saatavilla"}</div> 
+            : 
+            <Spinner animation="border" role="status" variant="dark" />
         }
       </Row>
-      <footer>
+      {/* Testing data overflow scroll to view */}
+      {/* {[1,2,3,4,5,6,7,8].map(el => <Row style={{height: "150px"}}>{el}</Row>)} */}
+    
+      <footer className="field" ref={fieldRef}>
         {data.fi && data.fi.Footer}
       </footer>
     </Container> 
